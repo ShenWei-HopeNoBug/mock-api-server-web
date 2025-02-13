0bce584a4732017a2a7c114af2890d821ae7ad2a
shenwei <sw18368337305@163.com>
2025-02-13 16:20:39 +0800

    预览数据源支持本地上传

--- apps/dataPreview/App.vue
+++ apps/dataPreview/App.vue
@@ -29,5 +29,6 @@ export default {
 .app {
   width: 100%;
   height: 100%;
+  padding: 20px;
 }
 </style>
--- apps/dataPreview/views/outputManager/index.vue
+++ apps/dataPreview/views/outputManager/index.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="output-manager">
-    <el-table :data="dataSource" highlight-current-row>
-      <el-table-column
-        v-for="(item, i) in tableColumns"
-        :key="`column-${i}`"
-        :prop="item.key"
-        :label="item.label"
-        v-bind="item.props"
-      />
-      <el-table-column label="操作" fixed="right" width="250">
-        <template slot-scope="scope">
-          <el-button type="text" @click="onDetail(scope.row)">查看详情</el-button>
+    <el-card v-if="!isHttpProtocol" class="header">
+      <UploadModel accept=".json" @upload="onUpload">
+        <template #content>
+          <el-button type="primary">选择本地抓包数据</el-button>
         </template>
-      </el-table-column>
-    </el-table>
+      </UploadModel>
+    </el-card>
+    <el-card class="content">
+      <el-table :data="dataSource" highlight-current-row>
+        <el-table-column
+          v-for="(item, i) in tableColumns"
+          :key="`column-${i}`"
+          :prop="item.key"
+          :label="item.label"
+          v-bind="item.props"
+        />
+        <el-table-column label="操作" fixed="right" width="250">
+          <template slot-scope="scope">
+            <el-button type="text" @click="onDetail(scope.row)">查看详情</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
     <DetailDialog ref="detailDialog" :data-source="curRow" @close="onDialogClose" />
   </div>
 </template>
@@ -23,10 +32,12 @@ import { cloneDeep } from 'lodash';
 import DetailDialog from './components/DetailDialog.vue';
 import { tableColumns } from './config';
 import { getMitmproxyOutputList } from 'apps/dataPreview/api';
+import UploadModel from 'src/components/UploadModel/index.vue';
+import { isJsonString } from 'src/assets/js/utils';
 
 export default {
   name: 'outputManager',
-  components: { DetailDialog },
+  components: { DetailDialog, UploadModel },
   data() {
     return {
       tableColumns,
@@ -52,6 +63,20 @@ export default {
     onDialogClose() {
       this.curRow = {};
     },
+    onUpload(file) {
+      if (!file) {
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target.result;
+        const dataSource = isJsonString(content) ? JSON.parse(content) : [];
+        this.dataSource = Array.isArray(dataSource) ? dataSource : [];
+      };
+
+      reader.readAsText(file);
+    },
   },
 };
 </script>
@@ -59,5 +84,21 @@ export default {
 <style scoped lang="less">
 .output-manager {
   width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .header {
+    width: 100%;
+    margin-bottom: 24px;
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
+  .content {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
